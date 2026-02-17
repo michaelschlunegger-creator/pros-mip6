cdbc19dea0811569919d1ac671456b11fa43a23b
michaelschlunegger-creator <michael.schlunegger@gmail.com>
2026-02-17 20:19:22 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/pros-mip/", // IMPORTANT: repo name for GitHub Pages project site
 });
