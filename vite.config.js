3831569a5c3f386c0bf48f867caec9f78eba14d2
michaelschlunegger-creator <michael.schlunegger@gmail.com>
2026-02-18 10:28:50 +0400

    Refactor vite.config.js to adjust plugin and base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
-  base: "/pros-mip/", // IMPORTANT: repo name for GitHub Pages project site
-});
+  base: "/pros-mip/",
+  plugins: [react()]
+})
